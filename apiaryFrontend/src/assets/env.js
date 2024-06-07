967d1d8dffcfc0d271a9b2d02468ab7bd98fa99e
Remigiusz Pisarski <RPisarski@private.convista.de>
2024-06-07 18:55:35 +0200

    m

---
+++ apiaryFrontend/src/assets/env.js
@@ -0,0 +1,7 @@
+(function (window) {
+    window['env'] = window['env'] || {};
+  
+    // Environment variables
+    window['env']['HIVE_URL'] = 'http://localhost:8080/api/hives';
+    window['env']['SEASON_URL'] = 'http://localhost:8080/api/seasons';
+})(this);
